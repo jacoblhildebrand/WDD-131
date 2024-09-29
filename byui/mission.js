e7b0817ccd9dab843bd247fb91e30a1e112f326d
jacoblhildebrand <jacob.l.hildebrand@gmail.com>
2024-09-28 18:35:39 -0600

    changed .webp to .png

--- byui/mission.js
+++ byui/mission.js
@@ -4,12 +4,12 @@ const themeSelector = document.querySelector('#theme-selector');
 // Function to change theme
 function changeTheme() {
     const body = document.body;
-    const logo = document.querySelector('footer img'); // Assuming the logo is in the footer
+    const logo = document.querySelector('footer img'); 
 
     if (themeSelector.value === 'dark') {
         // Apply dark theme
         body.classList.add('dark');
-        logo.src = 'images/byui-logo_white.webp'; // Change to white logo for dark theme
+        logo.src = 'images/byui-logo_white.png'; // Change to white logo for dark theme
     } else {
         // Apply light theme
         body.classList.remove('dark');
